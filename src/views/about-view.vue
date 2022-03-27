fc33718366553094e3c8ec8319fdd0ca47fcaf36
jim0409 <berserker.01.tw@hotmail.com>
2022-03-27 22:19:44 +0800

    misc: remove dangling render

--- src/views/about-view.vue
+++ src/views/about-view.vue
@@ -1,3 +1,16 @@
 <template>
+  <img alt="Vue logo" src="../assets/logo2.png">
   <h1>About Page</h1>
+  <HelloWorld msg="你想試試看 import 嗎!!"/>
 </template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  }
+}
+</script>
--- src/views/home-view.vue
+++ src/views/home-view.vue
@@ -2,21 +2,15 @@
   <img alt="Vue logo" src="../assets/logo.png">
   <h1>Home Page</h1>
   <HelloWorld msg="這邊會被import"/>
-  <HelloWorld msg2="這邊不會被import"/>
-  <HelloWorld msg3="這邊又被import啦"/>
-
-  <NiceToMeetYou msg="這邊好像也會被import"/>
 </template>
 
 <script>
 import HelloWorld from '../components/HelloWorld.vue'
-import NiceToMeetYou from '../components/NiceToMeetYou.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    NiceToMeetYou,
   }
 }
 </script>
