851e20c08a3c151984ec4960c96b972c8ad01f00
jim0409 <berkser.01.tw@hotmail.com>
2022-03-28 15:48:51 +0800

    misc: add sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <template>
-  <div id="nav">
+<Button icon="pi pi-arrow-right" @click="visibleLeft = true" >ClickMe</Button>
+  <SidebarView v-model:visible="visibleLeft">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-  </div>
+  </SidebarView>
   <router-view />
 
 </template>
 
+<script>
+import { ref } from 'vue';
+
+export default {
+    setup() {
+        const visibleLeft = ref(false);
+        const visibleRight = ref(false);
+        const visibleTop = ref(false);
+        const visibleBottom = ref(false);
+        const visibleFull = ref(false);
+
+		return { visibleLeft, visibleRight, visibleTop, visibleBottom, visibleFull }
+    }
+}
+</script>
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import PrimeVue from 'primevue/config';
+import SidebarView from 'primevue/sidebar';
+
+import "primevue/resources/primevue.min.css";
+import "primevue/resources/themes/saga-orange/theme.css";
 
 
 // 宣告一個 Vue App
 const app = createApp(App)
-app.use(store)
-app.use(router)
-app.mount('#app')
+app.use(PrimeVue, { ripple: true });
+
+app.use(store);
+app.component('SidebarView', SidebarView);
+app.use(router).mount('#app');
 
